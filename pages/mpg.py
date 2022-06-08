4bcdc9a3a4ed58fa1731175fba2b8e176756b759
sorijoo <48428325+sorijoo@users.noreply.github.com>
2022-06-08 16:19:11 +0900

    Create mpg.py

---
+++ pages/mpg.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
